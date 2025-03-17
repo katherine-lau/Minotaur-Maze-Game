d8418ace9f1bafd9a64b8c8a196fae626dbf18d1
Katherine Lau <katherine.lau.777@gmail.com>
2025-03-17 16:13:57 -0400

    Barebones, window created

--- maze.py
+++ maze.py
@@ -1,1 +1,22 @@
+import pygame
 
+WIDTH, HEIGHT = 800, 1000    #width and height of window
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))  #window
+pygame.display.set_caption("Minotaur Maze Game")
+
+def main(): #Where all the code is going to go
+    
+    #CODE BEFORE STARTUP GOES HERE
+    
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        #CODE After STARTUP GOES HERE
+    
+    pygame.quit()
+    
+if __name__ == "__main__":  #so game is only run when wanted, __name__ = name of file
+    main()
